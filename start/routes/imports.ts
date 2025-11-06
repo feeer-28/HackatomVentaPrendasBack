a4415e958d4dfd75e52fbd69334748dd9b8a6fda
feeer-28 <darcyfernandagonzalez@gmail.com>
2025-11-06 08:17:32 -0500

    rutas sin proteccion

--- start/routes/imports.ts
+++ start/routes/imports.ts
@@ -1,10 +1,10 @@
 import router from '@adonisjs/core/services/router'
-import { middleware } from '#start/kernel'
+//import { middleware } from '#start/kernel'
 const ImportController = () => import('#controllers/ImportController')
 
 router
   .group(() => {
     router.post('/productos', [ImportController, 'importarProductos'])
   })
-  .use([middleware.jwt()])
+  //.use([middleware.jwt()])
   .prefix('/api/imports')
--- start/routes/inventario.ts
+++ start/routes/inventario.ts
@@ -1,5 +1,5 @@
 import router from '@adonisjs/core/services/router'
-import { middleware } from '#start/kernel'
+//import { middleware } from '#start/kernel'
 const InventarioController = () => import('#controllers/InventarioController')
 
 router
@@ -10,5 +10,5 @@ router
     router.put('/actualizar/:id', [InventarioController, 'actualizar'])
     router.delete('/eliminar/:id', [InventarioController, 'eliminar'])
   })
-  .use([middleware.jwt()])
+  //.use([middleware.jwt()])
   .prefix('/inventario')
